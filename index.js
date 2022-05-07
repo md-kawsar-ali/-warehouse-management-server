1cfd877080dcf8014ab7ac9f7cb3406f40033265
mdkawsarali <facebookerjordan@gmail.com>
2022-05-08 01:48:56 +0600

    Get All cars

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,19 +9,34 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// Get API
+// Mongodb config
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.i1sr7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// Welcome
 app.get('/', (req, res) => {
-    res.send('Inventory Management Server Running!');
+    res.send('Welcome to the Inventory Management Server!');
 });
 
-app.get('/user', (req, res) => {
-    const user = {
-        name: 'md kawsar ali',
-        age: 22,
-        gender: 'male'
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("motodeal");
+        const carsCollection = database.collection("cars");
+
+        // Get All cars
+        app.get('/cars', async (req, res) => {
+            const query = {};
+            const cursor = carsCollection.find(query);
+            const result = await cursor.toArray();
+            res.json(result);
+        });
+
+    } finally {
+        //   await client.close();
     }
-    res.send(user);
-});
+}
+run().catch(console.dir);
 
 // Port Listener
 app.listen(port, () => {
